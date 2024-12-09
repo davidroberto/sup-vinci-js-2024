6cd041fdd0248ae7466d64b9c0056279e78e9a60
David Robert <contact@david-robert.fr>
2024-12-09 11:26:49 +0100

    variables with var: string, number, boolean

---
+++ script.js
@@ -0,0 +1,20 @@
+// je créé une variable avec le mot clé var
+// dont le nom est fullName
+// et la valeur est une chaine de caractères "David Robert";
+var fullName = "David Robert";
+// un égal permet de faire une "affectation"
+// c'est à dire stocker une valeur dans une variable
+var age = 34;
+var isAuthenticated = true;
+
+// je créé une variable nommée "langages" contenant un tableau,
+// avec dans le tableau 4 valeurs (4 chaine de caractères)
+var langages = ["PHP", "Javascript", "Rust", "react.js"];
+
+// je créé une condition qui execute le console.log, uniquement
+// si la variable isAuthenticated contient la valeur true
+// pour faire une comparaison (comparer deux valeurs / deux variables)
+// j'utilise le double ou triple égal
+if (isAuthenticated === true) {
+  console.log(fullName);
+}
